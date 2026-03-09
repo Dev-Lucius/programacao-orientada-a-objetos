d2c7b7b9948401f5aecd78c88b30269700c1e3f0
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-03-09 15:38:35 -0300

    Add new item to Pedido and update expected totals

--- poo_listas/poo_ex5/Main.java
+++ poo_listas/poo_ex5/Main.java
@@ -6,11 +6,14 @@ public class Main {
 
         p.adicionarItem(new ItemPedido("Café", 8.50, 2));
         p.adicionarItem(new ItemPedido("Pão", 1.50, 6));
+        p.adicionarItem(new ItemPedido("Leite", 5.60, 12));
 
-        System.out.println("Itens esperados = 2 | Atual = " + p.quantidadeDeItens());
-        System.out.println("Total esperado = 26.0 | Atual = " + p.total());
-        // 8.50*2 = 17.0
-        // 1.50*6 = 9.0
-        // total = 26.0
+        System.out.println("Itens esperados = 3 | Atual = " + p.quantidadeDeItens());
+        System.out.println("Total esperado = 93.0 | Atual = " + p.total());
+        
+        // 8.50 * 2 = 17.0
+        // 1.50 * 6 = 9.0
+        // 5.60 * 12 = 67.2
+        // total = 93
     }
 }
