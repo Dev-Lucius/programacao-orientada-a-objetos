59431036e88da51e6f04bc2dcf37f4bd233fec3f
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-03-10 17:33:40 -0300

    Implement basic functionality for CofreDigital

--- poo_aula2/typescript/Main.ts
+++ poo_aula2/typescript/Main.ts
@@ -1,1 +1,25 @@
-// To Do -> Implementar em TypeScript o que foi abordado em Java
+import CofreDigital from "./CofreDigital.js";
+
+const cofre = new CofreDigital();
+
+console.log(cofre);
+
+// Testando o Depósito
+cofre.Depositar(100);
+cofre.Depositar(-10);
+
+console.log(
+    "Saldo Esperado = 100 | Saldo Atual = " + cofre.Saldo()
+);
+
+const ok1 = cofre.Sacar(30);
+const ok2 = cofre.Sacar(500);
+
+console.log(
+    "Saque 30 ok? " + ok1 + " | Saque 500 ok? " + ok2
+);
+
+console.log(
+    "Saldo Esperado = 70 | Saldo Atual = " + cofre.Saldo()
+);
+
