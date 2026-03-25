6497b84a80923d116100d6302e14a4f39cbda98f
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-03-25 08:02:10 -0300

    Update Professor.java

--- poo_lista_2/poo_ex7/Professor.java
+++ poo_lista_2/poo_ex7/Professor.java
@@ -1,11 +1,57 @@
 package poo_ex7;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Professor {
 
     private int id;
     private String nome;
-    private List<Turma> turmas;
-    
+
+    // Um professor pode ter 1 ou várias turmas
+    private final List<Turma> turmas;
+
+    // GETTERS
+    public int getIdProfessor() {
+        return id;
+    }
+
+    public String getNomeProfessor() {
+        return nome;
+    }
+
+    public List<Turma> getTurmasProfessor() {
+        return turmas;
+    }
+
+
+    // SETTERS
+    public void setIdProfessor(int id){
+        if(id <= 0){
+            throw new IllegalArgumentException("Id inválido");
+        } else {
+            this.id = id;
+        }
+    }
+
+    public void setNomeProfessor(String nome){
+        if(nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome inválido");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public void adicionarTurma(Turma turma){
+        if(turma == null){
+            throw new IllegalArgumentException("Turma inválida");
+        } else {
+            turmas.add(turma);
+            turma.setProfessorTurma(this);
+        }
+    }
+
+    public void acessarTurma(){
+        // TO DO;
+    }
 }
