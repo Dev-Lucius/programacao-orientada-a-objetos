1ae40e786fee4eabb7f26bff3ac20bcff249cd3c
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-03-25 08:01:32 -0300

    Update Turma.java

--- poo_lista_2/poo_ex7/Turma.java
+++ poo_lista_2/poo_ex7/Turma.java
@@ -1,5 +1,55 @@
 package poo_ex7;
 
 public class Turma {
+
+    private int id;
+    private String nome;
+    private Professor professor;
+
+    // GETTERS
+    public int getIdTurma() {
+        return id;
+    }
+    public String getNomeTurma() {
+        return nome;
+    }
     
+    // SETTERS
+    public void setIdTurma(int id){
+        if(id <= 0){
+            throw new IllegalArgumentException("Id inválido");
+        } else{
+            this.id = id;
+        }
+    }
+
+    public void setNomeTurma(String nome){
+        if(nome == null || nome.trim().isEmpty()){
+            throw new IllegalArgumentException("Nome inválido");
+        } else {
+            this.nome = nome;
+        }
+    }
+
+    public void setProfessorTurma(Professor professor){
+        this.professor = professor;
+    }
+
+    /**
+     * CONSTRUTOR DA TURMA
+     * @param id -> Identificador de Turma
+     * @param nome -> Nome da Turma
+     * professor começa null, porém é modificado via um setter
+     */
+    public Turma(int id, String nome){
+        setIdTurma(id);
+        setNomeTurma(nome);
+    }
+
+    @Override
+    public String toString(){
+        return  "Turma #" + id + 
+                " | Nome: " + nome + 
+                " | Professor: " + (professor != null ? professor : "Sem professor");
+    }
 }
