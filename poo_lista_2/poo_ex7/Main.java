91f432077015b6c0d65f498a40c0b5c118e73190
Lucas Oliveira <147167871+Dev-Lucius@users.noreply.github.com>
2026-03-25 08:01:49 -0300

    Update Main.java

--- poo_lista_2/poo_ex7/Main.java
+++ poo_lista_2/poo_ex7/Main.java
@@ -2,6 +2,21 @@ package poo_ex7;
 
 public class Main {
     public static void main(String[] args) {
+
+        Professor pf = new Professor(1, "Márcio");
+
+        Turma t1 = new Turma(1, "Matemática");
+        Turma t2 = new Turma(2, "Física");
+        Turma t3 = new Turma(1, "Química");
+
+        pf.adicionarTurma(t1);
+        pf.adicionarTurma(t2);
+        pf.adicionarTurma(t3);
+
+        pf.acessarTurma(t1);
+        pf.acessarTurma(t2);
+        pf.acessarTurma(t3);
+
         
     }
 }
